d83101eb2c8bde37a003318fc088df3f8acaec81
Your Name <you@example.com>
2025-06-14 15:45:41 +0000

    646

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+result = ""
+def uppercase(str):
+    for i in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    return result
