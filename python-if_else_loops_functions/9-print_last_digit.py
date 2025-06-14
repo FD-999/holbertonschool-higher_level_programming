664fafe93b30107f8b335f53105d8e8b528e089e
Your Name <you@example.com>
2025-06-14 15:54:18 +0000

    646

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number > 0:
-        return number % 10
+        a = number%10
+        print("{}".format(a))
     else:
-        return -(number % 10)
+        a = -(number%10)
+        print("{}".format(a))
