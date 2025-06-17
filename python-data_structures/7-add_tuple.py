6a2eb6a8d4fff781a65789710ba80f05dfd6350a
Your Name <you@example.com>
2025-06-17 16:00:38 +0000

    646

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = []
+    for i in tuple_a:
+        new_tuple += i:
+    for j in tuple_b:
+        new_tuple += j:
+    return new_tuple
