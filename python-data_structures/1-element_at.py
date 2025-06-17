6f424fc5c2718eee061604420d744be7d9f9515b
Your Name <you@example.com>
2025-06-17 11:26:30 +0000

    55

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,6 +3,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
     else:
-        num = my_list[idx]
-        print(f"Element at index {idx} is {num}")
-        
+        return my_list[idx]
