f22d0d74bdf30c2c6fe8cc58a3cc06a035d9ac41
Your Name <you@example.com>
2025-06-17 14:47:29 +0000

    646

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     k = len(my_list) - 1
     for i in range(len(my_list)):
-        print("{:d}".format(mylist[k]))
+        print("{:d}".format(my_list[k]))
         k -= 1
