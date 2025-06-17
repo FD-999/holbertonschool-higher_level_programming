d26e4e410f8eb2798cb677cd6a4adc0b70150a01
Your Name <you@example.com>
2025-06-17 16:18:39 +0000

    546

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,4 +3,6 @@ def multiple_returns(sentence):
     s = 0
     for i in sentence:
         s += 1
+        if s == 0:
+            print(f"Length: {s} - First character: None")
     print(f"Length: {s} - First character: {sentence[0]}")
