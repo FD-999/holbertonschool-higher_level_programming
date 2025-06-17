c913679c3ce719b13fbb3f6d88c3ed7924023cd2
Your Name <you@example.com>
2025-06-17 15:27:23 +0000

    646

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}".format(j), end=" ")
+            if i != len(i) - 1:
+                print("{:d}".format(j), end=" ")
+            else:
+                print("{:d}".format(j))
         print(end="\n")
+
