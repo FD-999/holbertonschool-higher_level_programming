d5bbf3dba4ced999f7719c62df079eebe7880676
Your Name <you@example.com>
2025-06-17 16:33:18 +0000

    546

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = []
     if len(my_list) == 0:
         return None
     else:
         my_list.sort()
-        max_value.extend(my_list)
-        print("Max: {}".format(max_value[-1])
+        print("Max: {}".format(my_list[-1]))
