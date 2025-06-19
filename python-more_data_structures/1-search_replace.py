442ef68475fa51cb5b19e5ea269b91ff57de1895
Your Name <you@example.com>
2025-06-19 13:03:31 +0000

    6546

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
-        new_num = int(str(i).replace("search", "replace"))
+        new_num = int(str(i).replace(search, replace))
         new_list.append(new_num)
     return new_list
