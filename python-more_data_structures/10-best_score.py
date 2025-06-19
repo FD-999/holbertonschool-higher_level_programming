2e62ecb81541a38b0cef48498565021240202011
Your Name <you@example.com>
2025-06-19 16:47:20 +0000

    6546

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    else:
-        return max(a_dictionary.values())
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return max_key
+        
