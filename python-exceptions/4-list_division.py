858dd7a6e25ea73354e04a0eebc2f8ef752431ef
Your Name <you@example.com>
2025-06-21 14:18:32 +0000

    64

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    length = list_length
-    for i in range(length):
+    for i in range(list_length):
         try:
             div =  my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
